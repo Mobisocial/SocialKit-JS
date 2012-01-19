00b451714ac5ef6e3933c9ed1ffe72de6c7f8261
Ben James Dodson <bjdodson@suif.stanford.edu>
2012-01-18 18:07:51 -0800

    BIG WORDS\!\!

---
+++ apps/mobisocial.bigwords/app.js
@@ -0,0 +1,40 @@
+/*
+ * TicTacToe is the application's main class
+ */
+function MusuWriter(app) {
+  this.appContext = app;
+}
+
+var musu;
+Musubi.ready(function(context) {
+    console.log("launching musuwriter");
+    musu = new MusuWriter(context);
+
+    $("#post").click(function(e) {
+      var style = "font-size:30px;";
+      style += " background-color:" + $("#textbox").css("background-color") + ";";
+      style += " color:" + $("#textbox").css("color") + ";";
+
+      var html = '<span style="' + style + '">' + $("#textbox").val() + '!!</span>';
+      var content = { "__html" : html };
+      var obj = new SocialKit.Obj({type : "note", data: content})
+      musu.appContext.feed.post(obj);
+      $("body").html(html);
+    });
+});
+
+$(function(){
+  $("#background div").each(function(i, v){
+    $(v).css("background-color", $(v).attr("color"));
+    $(v).click(function(e) {
+      $("#textbox").css("background-color", $(e.target).attr("color"));
+    });
+  });
+
+  $("#foreground div").each(function(i, v){
+    $(v).css("background-color", $(v).attr("color"));
+    $(v).click(function(e) {
+      $("#textbox").css("color", $(e.target).attr("color"));
+    });
+  });
+});
